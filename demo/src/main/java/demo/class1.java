3fc771463b8d04eab111aedb0f022882be2ddba4
Administrator <Administrator@N12931.intellectdesign.com>
2021-09-16 16:54:27 +0530

    done

--- demo/src/main/java/demo/class1.java
+++ demo/src/main/java/demo/class1.java
@@ -4,7 +4,8 @@ public class class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("welcome");
+System.out.println("welldone");
+System.out.println("hii");
 	}
 
 }
